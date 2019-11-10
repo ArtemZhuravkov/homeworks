ec2f51e1856fd05af110c639d377b17a60d78180
Artem Zhuravkov <navy2494@gmail.com>
2019-11-10 17:34:32 +0200

    end a guess task

--- dz6/script.js
+++ dz6/script.js
@@ -1,10 +1,17 @@
-var random = Math.ceil(Math.random() * 5),
-    guess;
-    
-do{
-    guess = prompt('Guess y\'re number');    
-} while (guess !== null && +guess !== random);
+// Угадать число do\while
 
-if(guess !== null){
-    alert ('you guessed a number!');
-}
+// var random = Math.ceil(Math.random() * 5),
+//     guess;
+    
+// do{
+//     guess = prompt('Guess y\'re number');    
+// } while (guess !== null && +guess !== random);
+
+// if(guess !== null){
+//     alert ('you guessed a number!');
+// }
+
+// for(var random = Math.ceil(Math.random() * 5), guess; guess !== null && +guess !== random; guess = prompt('Guess y\'re number'));
+// if(guess !== null){
+//     alert ('you guessed a number!');
+// }
