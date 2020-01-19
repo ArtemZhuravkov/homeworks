9de13bfc7dfcae2466ba05f2e6b592155c2619ad
Artem Zhuravkov <navy2494@gmail.com>
2020-01-19 17:09:47 +0200

    add dz11

--- dz11/script.js
+++ dz11/script.js
@@ -1,34 +1,33 @@
-// var arr = [1, 2, 3, 4, -5];
-// function some(arr, cb) {
-//     for (let index = 0; index < array.length; index++) {
-//         if(!cb(arr[index], index, arr)) {
-//             return true;
-//         }        
-//     }
-// }
-
-// var newArr = arr.some(function(value){
-//     return value > 2
-// });
-// console.log(newArr);
-
-function fill(arr, cb) {
-    
-    var newArr = [];    
-    for (let index = 0; index < arr.length; index++) {
-        if(cb(arr[index], index, arr) === true){
-            return arr[index] ;
-        }                            
-    } 
-    return newArr;     
-}
 var arr = [1, 2, 3, 4, -5];
-var newArr = fill(arr, function (value){
+function some(arr, cb) {
+    for (let index = 0; index < array.length; index++) {
+        if(!cb(arr[index], index, arr)) {
+            return true;
+        }        
+    }
+}
+
+var newArr = arr.some(function(value){
     return value > 2
 });
+console.log(newArr);
 
 
 
-console.log(arr, newArr);
+
+function fill(arr, cb) {    
+    var newArr = [];   
+    for (var index = 0; index < arr.length; index++) {
+        if(cb(arr[index], index, arr)){
+            newArr.push(arr[index]); 
+        }                                                
+    }
+    return newArr;
+}
 
 
+var arr = ['artem', 'ivan', 'roman', 'lev', 'inna'];
+var result = fill(arr, function(word){
+    return word.length > 3
+});
+console.log(result);
